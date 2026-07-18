dabe3a644074691d8c0b30e6f225b87c8b8b2ab1
Akash Chakraborty <chakrabortyakash731@gmail.com>
2026-07-18 17:37:18 +0530

    util folders code

--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 require('dotenv').config({ path: './.env' });
 const connectDB = require('./db/index.js');
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000, () => {
+        console.log(`Server is running on port ${process.env.PORT || 8000}`);
+    });
+})
+.catch((err) => {
+    console.log(err.message);
+})
